e2479a412c2fec7d454ec90ba2dd8668699d62e3
Joost Nijhuis <jnijhuis81@gmail.com>
2019-05-01 21:21:28 +0200

    Added some test classes which can be used in the tests.

---
+++ tests/Entity/TestEntity.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of the jojo1981/typed-collection package
+ *
+ * Copyright (c) 2019 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+namespace tests\Jojo1981\TypedCollection\Entity;
+
+/**
+ * @package tests\Jojo1981\TypedCollection\Entity
+ */
+class TestEntity extends TestEntityBase
+{
+}
