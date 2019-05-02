d49527233a6e4795ab3c7b35fb041b650aaa28c4
Joost Nijhuis <jnijhuis81@gmail.com>
2019-05-02 15:17:18 +0200

    Add tests for `SuccessValidationResult`

---
+++ tests/Value/Validation/SuccessValidationResultTest.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of the jojo1981/typed-collection package
+ *
+ * Copyright (c) 2019 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+namespace tests\Jojo1981\TypedCollection\Value\Validation;
+
+use Jojo1981\TypedCollection\Value\Validation\SuccessValidationResult;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+
+/**
+ * @package tests\Jojo1981\TypedCollection\Value\Validation
+ */
+class SuccessValidationResultTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @throws InvalidArgumentException
+     * @throws ExpectationFailedException
+     * @return void
+     */
+    public function isValidShouldReturnTrue(): void
+    {
+        $this->assertTrue((new SuccessValidationResult())->isValid());
+    }
+
+    /**
+     * @test
+     *
+     * @throws InvalidArgumentException
+     * @throws ExpectationFailedException
+     * @return void
+     */
+    public function getMessageShouldReturnNull(): void
+    {
+        $this->assertNull((new SuccessValidationResult())->getMessage());
+    }
+}
